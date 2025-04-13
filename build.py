04413ef6cc8c93bdc89346901c89210e47c7c6bd
Pablo Torralbo Martinez <ptm00017@red.ujaen.es>
2025-04-13 21:00:45 +0200

    Añadir script para compilar el juego

---
+++ build.py
@@ -0,0 +1,23 @@
+from cx_Freeze import setup, Executable
+
+# Archivos adicionales (como recursos)
+include_files = ['res/']
+
+# Configuración del ejecutable
+executables = [
+    Executable("main.py", base="Win32GUI", target_name="suika.exe")
+]
+
+# Setup de cx_Freeze
+setup(
+    name="Suika Game",
+    version="1.0",
+    description="Proyecto de la asignatura de Sistemas Multimedia de la UJA",
+    options={
+        "build_exe": {
+            "include_files": include_files,
+            "build_exe": "build/output",  # Usamos una carpeta temporal para los archivos de construcción
+        }
+    },
+    executables=executables
+)
